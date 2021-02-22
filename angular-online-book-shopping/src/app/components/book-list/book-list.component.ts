a93ec4eed6ce00e7dd8f3f4c745832b156440f9d
vivekkumar333 <tiwari.vivekkumar333@gmail.com>
2021-02-22 08:44:46 +0530

    Created book service in angular application & Enable cross origin support for default port : 4200

--- angular-online-book-shopping/src/app/components/book-list/book-list.component.ts
+++ angular-online-book-shopping/src/app/components/book-list/book-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from 'src/app/common/book';
+import { BookService } from 'src/app/services/book.service';
 
 
 @Component({
@@ -9,47 +10,18 @@ import { Book } from 'src/app/common/book';
 })
 export class BookListComponent implements OnInit {
 
-  books: Book[] = [
-    {
-      "sku": "text-100",
-      "name": "C IN Depth",
-      "description": "Concept of C Language",
-      "unitPrice": 350,
-      "imageUrl": "assests/images/book/boo101.jpg",
-      "active": true,
-      "unitsInStock": 9,
-      "createdOn": new Date(),
-      "updatedOn": null
-    },
-
-    {
-      "sku": "text-101",
-      "name": "Let US C",
-      "description": "Concept of C Language",
-      "unitPrice": 300,
-      "imageUrl": "assests/images/book/boo102.jpg",
-      "active": true,
-      "unitsInStock": 250,
-      "createdOn": new Date(),
-      "updatedOn": null
-    },
-
-    {
-      "sku": "text-103",
-      "name": "Ramayna",
-      "description": "Hindus Oldest Scripture",
-      "unitPrice": 1999,
-      "imageUrl": "assests/images/book/boo103.jpg",
-      "active": true,
-      "unitsInStock": 55,
-      "createdOn": new Date(),
-      "updatedOn": null
-    }
-
-  ]
-  constructor() { }
+  books: Book[];
+  constructor(private _bookService: BookService) { }
 
   ngOnInit(): void {
+    this.listBooks();
   }
 
+  listBooks(){
+    this._bookService.getBooks().subscribe(
+      data =>{
+        console.log(data);
+      }
+    )
+  }
 }
